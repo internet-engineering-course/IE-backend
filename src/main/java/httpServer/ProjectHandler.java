876fc814b16ef026fd89b6be49cb4e7a8ce072f3
navid <anavid.akbari@gmail.com>
2019-02-22 15:08:01 +0330

    this commit will add project handler HTML content

--- src/main/java/httpServer/ProjectHandler.java
+++ src/main/java/httpServer/ProjectHandler.java
@@ -16,5 +16,44 @@ public class ProjectHandler implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         //show project that user can satisfy
         List<Project> valideProjects = Commands.getValidProjects(Commands.getDefaultUser());
+        String response ="\n" +
+                "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Projects</title>\n" +
+                "    <style>\n" +
+                "        table {\n" +
+                "            text-align: center;\n" +
+                "            margin: 0 auto;\n" +
+                "        }\n" +
+                "        td {\n" +
+                "            min-width: 300px;\n" +
+                "            margin: 5px 5px 5px 5px;\n" +
+                "            text-align: center;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    <table>\n" +
+                "        <tr>\n" +
+                "            <th>id</th>\n" +
+                "            <th>title</th>\n" +
+                "            <th>budget</th>\n" +
+                "        </tr>\n";
+        for(int i= 0 ; i < valideProjects.size() ; i++) {
+            response +="        <tr>\n" +
+                    "            <td>"+i+"</td>\n" +
+                    "            <td>"+valideProjects.get(i).getTitle()+"</td>\n" +
+                    "            <td>"+valideProjects.get(i).getBudget()+"</td>\n" +
+                    "        </tr>\n";
+        }
+        response +="    </table>\n" +
+                "</body>\n" +
+                "</html>";
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
